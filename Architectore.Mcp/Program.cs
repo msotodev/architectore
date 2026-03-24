fda4493cde85e76227007e44fc6803c7275c5af9
msotodev <mariosotomor.dev@gmail.com>
2026-03-23 21:34:15 -0600

    feat: New Mcp project to call builders

---
+++ Architectore.Mcp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Architectore.Mcp
+{
+	public static class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+			builder.Logging.AddConsole(l => l.LogToStandardErrorThreshold = LogLevel.Trace);
+
+			builder.Services.AddMcpServer().
+				WithStdioServerTransport().
+				WithToolsFromAssembly();
+
+			await builder.Build().RunAsync();
+		}
+	}
+}
---
+++ Architectore.Mcp/Tools/ServiceTool.cs
@@ -0,0 +1,25 @@
+﻿using Architectore.Cli.Builders;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace Architectore.Mcp.Tools
+{
+	[McpServerToolType]
+	public static class ServiceTool
+	{
+		[McpServerTool, Description("Build a service using clean architecture")]
+		public static Task BuildServiceAsync(
+			string projectPath, string entity, string contracts
+		) => ServiceBuilder.BuildAsync(projectPath, entity, contracts);
+
+		[McpServerTool, Description("Build a repository using clean architecture")]
+		public static Task BuildRepositoryAsync(
+			string projectPath, string entity, string contracts
+		) => RepositoryBuilder.BuildAsync(projectPath, entity, contracts);
+
+		[McpServerTool, Description("Build a CQRS query using clean architecture")]
+		public static Task BuildQueryAsync(
+		string projectPath, string entity, string contracts
+	) => QueryBuilder.BuildAsync(projectPath, entity, contracts);
+	}
+}
